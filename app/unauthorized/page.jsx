cfd825c1dcaebb8f07466cf180b1b106b0750aed
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-05-13 16:34:37 +0600

    Unauthorize page

---
+++ app/unauthorized/page.jsx
@@ -0,0 +1,10 @@
+const Unauthorized = () => {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-red-600">Access Denied</h1>
+      <p className="mt-2 text-gray-700">You do not have permission to view this page.</p>
+    </div>
+  )
+}
+
+export default Unauthorized
