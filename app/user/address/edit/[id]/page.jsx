a09d153b48bab4beb7d9cc76325ca9e735090811
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-05-14 12:31:58 +0600

    Address edit page

---
+++ app/user/address/edit/[id]/page.jsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { useEffect, useState } from 'react'
+import api from '@/utils/axios';
+import { useRouter, useParams } from 'next/navigation'
+import toast from "react-hot-toast";
+import { Plus, Pencil, Trash2 } from "lucide-react";
+
+const EditAddress = () => {
+    const [form, setForm] = useState({
+        name: '',
+        address_line1: '',
+        address_line2: '',
+        city: '',
+        state: '',
+        pin_code: '',
+        country: ''
+    })
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const router = useRouter()
+    const params = useParams()
+
+    useEffect(() => {
+        const fetchAddress = async () => {
+            try {
+                const response = await api.get(`/api/shippings/addresses/${params.id}`)
+                setForm(response.data)
+            } catch(error) {
+                setError('Failed to load address');
+            }
+        }
+        fetchAddress()
+    }, [params.id])
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        try {
+            await api.patch(`/api/shippings/addresses/${params.id}`, form)
+            toast.success("Address updated successfully");
+            setTimeout(() => {
+                router.push("/user/address");
+            }, 1000);
+        } catch(err) {
+            setError(err.response?.data?.detail || 'Failed to update address')
+            toast.error(
+                err.response?.data?.detail || "Failed to save address"
+            );
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="p-6 max-w-xl mx-auto">
+            <h2 className="text-2xl font-bold mb-4"><Pencil size={16} className='inline'/> Edit Address</h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                <label className="block mb-1 font-medium">Name *</label>
+                <input
+                    name="name"
+                    value={form.name}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">Address Line 1 *</label>
+                <input
+                    name="address_line1"
+                    value={form.address_line1}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">Address Line 2</label>
+                <input
+                    name="address_line2"
+                    value={form.address_line2}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">City *</label>
+                <input
+                    name="city"
+                    value={form.city}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">State *</label>
+                <input
+                    name="state"
+                    value={form.state}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">Pin Code *</label>
+                <input
+                    name="pin_code"
+                    value={form.pin_code}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+                <div>
+                <label className="block mb-1 font-medium">Country *</label>
+                <input
+                    name="country"
+                    value={form.country}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+                </div>
+
+                {error && <p className="text-red-600 text-sm">{error}</p>}
+
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className={`px-4 py-2 rounded text-white cursor-pointer ${
+                        loading ? 'bg-gray-400' : 'bg-green-600 hover:bg-green-700'
+                    }`}
+                    >
+                    {loading ? 'Saving...' : 'Save Address'}
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default EditAddress
